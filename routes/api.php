a2abad8c48994b840a5d0e3ea2fcff62f8c2d6f6
den-jk33 <39164594+den-jk33@users.noreply.github.com>
2022-06-01 13:41:47 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,3 @@ use Illuminate\Support\Facades\Route;
 use App\Services\JsonRpcServer;
 use App\Http\Controllers\ActivityController;
 
-
-//Route::post('/activity/save', ['as' => 'index','uses' => 'ActivityController@saveNew']);
-//Route::get('/activity/list', ['as' => 'index','uses' => 'ActivityController@getList']);
-
-Route::middleware('auth:api')->post('/activity/data', function (Request $request, JsonRpcServer $server, ActivityController $controller) {
-    return $server->handle($request,$controller);
-});
